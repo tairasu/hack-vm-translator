43515f0cde61f3a5b9eeaf50d2354060604b9251
aomizu <marco.tylus24@gmail.com>
2024-05-24 18:08:25 +0900

    refactoring, minimized String type usage

--- src/lib.rs
+++ src/lib.rs
@@ -46,36 +46,12 @@ pub mod load {
 
 //instructions
 pub mod instructions {
-    pub fn add() -> String {
-        //addition
-        let mut asm = String::new();
-        asm.push_str("@SP\n");
-        asm.push_str("M=M-1\n");
-        asm.push_str("A=M\n");
-        asm.push_str("D=M\n");
-        asm.push_str("@SP\n");
-        asm.push_str("M=M-1\n");
-        asm.push_str("A=M\n");
-        asm.push_str("M=M+D\n");
-        asm.push_str("@SP\n");
-        asm.push_str("M=M+1\n");
-        asm
+    pub fn add() -> &'static str{
+        "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=M+D\n@SP\nM=M+1\n"
     }
     
-    pub fn sub() -> String {
-        //subtraction
-        let mut asm = String::new();
-        asm.push_str("@SP\n");
-        asm.push_str("M=M-1\n");
-        asm.push_str("A=M\n");
-        asm.push_str("D=M\n");
-        asm.push_str("@SP\n");
-        asm.push_str("M=M-1\n");
-        asm.push_str("A=M\n");
-        asm.push_str("M=M-D\n");
-        asm.push_str("@SP\n");
-        asm.push_str("M=M+1\n");
-        asm
+    pub fn sub() -> &'static str{
+        "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=M-D\n@SP\nM=M+1\n"
     }
 
     static mut LABEL_COUNT: usize = 0;
@@ -88,238 +64,148 @@ fn unique_label(prefix: &str) -> String {
     }
 }
 
-pub fn neg() -> String {
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=-M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+pub fn neg() -> &'static str {
+    "@SP\nM=M-1\nA=M\nM=-M\n@SP\nM=M+1\n"
 }
 
-pub fn not() -> String {
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=!M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+pub fn not() -> &'static str{
+    "@SP\nM=M-1\nA=M\nM=!M\n@SP\nM=M+1\n"
 }
 
-pub fn and() -> String {
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=D&M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+pub fn and() -> &'static str{
+    "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=D&M\n@SP\nM=M+1\n"
 }
 
-pub fn or() -> String {
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=D|M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+pub fn or() -> &'static str{
+    "@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=D|M\n@SP\nM=M+1\n"
 }
 
 pub fn eq() -> String {
     let label_true = unique_label("EQ_TRUE");
     let label_end = unique_label("EQ_END");
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M-D\n");
-    asm.push_str(&format!("@{}\n", label_true));
-    asm.push_str("D;JEQ\n");
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=0\n");
-    asm.push_str(&format!("@{}\n", label_end));
-    asm.push_str("0;JMP\n");
-    asm.push_str(&format!("({})\n", label_true));
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=-1\n");
-    asm.push_str(&format!("({})\n", label_end));
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+    format!(
+        "@SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M\n\
+        @SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M-D\n\
+        @{}\n\
+        D;JEQ\n\
+        @SP\n\
+        A=M\n\
+        M=0\n\
+        @{}\n\
+        0;JMP\n\
+        ({})\n\
+        @SP\n\
+        A=M\n\
+        M=-1\n\
+        ({})\n\
+        @SP\n\
+        M=M+1\n",
+        label_true, label_end, label_true, label_end
+    )
 }
 
 pub fn gt() -> String {
     let label_true = unique_label("GT_TRUE");
     let label_end = unique_label("GT_END");
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M-D\n");
-    asm.push_str(&format!("@{}\n", label_true));
-    asm.push_str("D;JGT\n");
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=0\n");
-    asm.push_str(&format!("@{}\n", label_end));
-    asm.push_str("0;JMP\n");
-    asm.push_str(&format!("({})\n", label_true));
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=-1\n");
-    asm.push_str(&format!("({})\n", label_end));
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+    format!(
+        "@SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M\n\
+        @SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M-D\n\
+        @{}\n\
+        D;JGT\n\
+        @SP\n\
+        A=M\n\
+        M=0\n\
+        @{}\n\
+        0;JMP\n\
+        ({})\n\
+        @SP\n\
+        A=M\n\
+        M=-1\n\
+        ({})\n\
+        @SP\n\
+        M=M+1\n",
+        label_true, label_end, label_true, label_end
+    )
 }
 
 pub fn lt() -> String {
     let label_true = unique_label("LT_TRUE");
     let label_end = unique_label("LT_END");
-    let mut asm = String::new();
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M\n");
-    asm.push_str("@SP\n");
-    asm.push_str("M=M-1\n");
-    asm.push_str("A=M\n");
-    asm.push_str("D=M-D\n");
-    asm.push_str(&format!("@{}\n", label_true));
-    asm.push_str("D;JLT\n");
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=0\n");
-    asm.push_str(&format!("@{}\n", label_end));
-    asm.push_str("0;JMP\n");
-    asm.push_str(&format!("({})\n", label_true));
-    asm.push_str("@SP\n");
-    asm.push_str("A=M\n");
-    asm.push_str("M=-1\n");
-    asm.push_str(&format!("({})\n", label_end));
-    asm.push_str("@SP\n");
-    asm.push_str("M=M+1\n");
-    asm
+    format!(
+        "@SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M\n\
+        @SP\n\
+        M=M-1\n\
+        A=M\n\
+        D=M-D\n\
+        @{}\n\
+        D;JLT\n\
+        @SP\n\
+        A=M\n\
+        M=0\n\
+        @{}\n\
+        0;JMP\n\
+        ({})\n\
+        @SP\n\
+        A=M\n\
+        M=-1\n\
+        ({})\n\
+        @SP\n\
+        M=M+1\n",
+        label_true, label_end, label_true, label_end
+    )
 }
     
     //push function for constant, local, argument, this, that, temp, pointer, static
     pub fn push(segment: &str, index: &str) -> String {
-        let mut asm = String::new();
+        let mut asm = String::with_capacity(100); // Preallocate with an estimated capacity
+        
         match segment {
             "constant" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "local" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@LCL\n");
-                asm.push_str("A=M+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@LCL\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "argument" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@ARG\n");
-                asm.push_str("A=M+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@ARG\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "this" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@THIS\n");
-                asm.push_str("A=M+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@THIS\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "that" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@THAT\n");
-                asm.push_str("A=M+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@THAT\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "temp" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@5\n");
-                asm.push_str("A=A+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@5\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "pointer" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@3\n");
-                asm.push_str("A=A+D\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}\n", index));
+                asm.push_str("D=A\n@3\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             "static" => {
-                asm.push_str(format!("@{}.{}\n", "test", index).as_str());
-                asm.push_str("D=M\n");
-                asm.push_str("@SP\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M+1\n");
+                asm.push_str(&format!("@{}.{}\n", "test", index));
+                asm.push_str("D=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
             }
             _ => {
                 println!("Invalid segment");
@@ -330,105 +216,36 @@ pub fn lt() -> String {
     
     //pop function for local, argument, this, that, temp, pointer, static
     pub fn pop(segment: &str, index: &str) -> String {
-        let mut asm = String::new();
+        let mut asm = String::with_capacity(100); // Preallocate with an estimated capacity
+        
         match segment {
             "local" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@LCL\n");
-                asm.push_str("D=M+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@LCL\nD=M+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "argument" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@ARG\n");
-                asm.push_str("D=M+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@ARG\nD=M+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "this" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@THIS\n");
-                asm.push_str("D=M+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@THIS\nD=M+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "that" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@THAT\n");
-                asm.push_str("D=M+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@THAT\nD=M+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "temp" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@5\n");
-                asm.push_str("D=A+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@5\nD=A+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "pointer" => {
-                asm.push_str(format!("@{}\n", index).as_str());
-                asm.push_str("D=A\n");
-                asm.push_str("@3\n");
-                asm.push_str("D=A+D\n");
-                asm.push_str("@R13\n");
-                asm.push_str("M=D\n");
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str("@R13\n");
-                asm.push_str("A=M\n");
-                asm.push_str("M=D\n");
+                asm.push_str(&format!("@{}\nD=A\n@3\nD=A+D\n@R13\nM=D\n", index));
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n");
             }
             "static" => {
-                asm.push_str("@SP\n");
-                asm.push_str("M=M-1\n");
-                asm.push_str("A=M\n");
-                asm.push_str("D=M\n");
-                asm.push_str(format!("@{}.{}\n", "test", index).as_str());
-                asm.push_str("M=D\n");
+                asm.push_str("@SP\nM=M-1\nA=M\nD=M\n");
+                asm.push_str(&format!("@{}.{}\nM=D\n", "test", index));
             }
             _ => {
                 println!("Invalid segment");
@@ -444,10 +261,10 @@ pub fn lt() -> String {
         let mut parsed = String::new();
         match command {
             "add" => {
-                parsed.push_str(add().as_str());
+                parsed.push_str(add());
             }
             "sub" => {
-                parsed.push_str(sub().as_str());
+                parsed.push_str(sub());
             }
             "push" => {
                 let segment = split_line.next().unwrap();
@@ -460,16 +277,16 @@ pub fn lt() -> String {
                 parsed.push_str(pop(segment, index).as_str());
             }
             "neg" => {
-                parsed.push_str(neg().as_str());
+                parsed.push_str(neg());
             }
             "not" => {
-                parsed.push_str(not().as_str());
+                parsed.push_str(not());
             }
             "and" => {
-                parsed.push_str(and().as_str());
+                parsed.push_str(and());
             }
             "or" => {
-                parsed.push_str(or().as_str());
+                parsed.push_str(or());
             }
             "eq" => {
                 parsed.push_str(eq().as_str());
